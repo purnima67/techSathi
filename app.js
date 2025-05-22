185d385689861d0f2f54241484a6d4f8777b2ede
AnujaLamichhane24 <lamichhaneanuja24@gmail.com>
2025-05-22 13:57:22 +0545

    changed in events

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bookRouter = require('./routes/book');
+var eventsRouter = require('./routes/events'); 
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));//new
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/book', bookRouter);
+app.use('/events', eventsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
