1d36b2a1c955a4dd0c07231e4bb2652c0c507683
purnima <purnimaw3@gmail.com>
2025-05-22 10:49:25 +0545

    initial commit

---
+++ models/book.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const BookSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Please enter the book title"],
+        },
+        author: {
+            type: String,
+            required: [true, "Please enter the book author"],
+        },
+        description: {
+            type: String,
+            required: [true, "Please enter the book description"],
+        },
+        genre: {
+            type: String,
+            required: [true, "Please enter the book genre"],
+        },
+  },
+  {
+    timestamps: true,
+  }
+)
+const Book = mongoose.model("Book", BookSchema)
+module.exports = Book
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
